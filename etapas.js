496588e1851c552321088addf2e0b8d7c7128911
Jomar A. França <jomar.almeida@gmail.com>
2022-05-19 15:35:37 -0300

    Terminando a estilização do conteúdo

--- etapas.js
+++ etapas.js
@@ -4,19 +4,19 @@ let etapas = [
 		numeros: 5,
 		candidatos: [
 			{
-				numero: '38111',
-				nome: 'Fulano de Tal',
-				partido: 'ABC',
+				numero: '77111',
+				nome: 'Helena da Silva',
+				partido: 'TdoB',
 				fotos: [
-					{url: 'mulher-begnus.jpg', legenda: 'Vereador'}
+					{url: '6.png', legenda: 'Vereadora'}
 				]
 			},
 			{
-				numero: '77222',
-				nome: 'Beltrano da Silva',
-				partido: 'DEFG',
+				numero: '88722',
+				nome: 'Marcelo Sousa',
+				partido: 'ADEBRA',
 				fotos: [
-					{url: 'filha-adrian.jpg', legenda: 'Vereador'}
+					{url: '1.png', legenda: 'Vereador'}
 				]   
 			},
 		]
@@ -26,23 +26,23 @@ let etapas = [
 		numeros: 2,
 		candidatos: [
 			{
-				numero: '99',
-				nome: 'Ciclano',
-				partido: 'ABC',
-				vice: 'CIC',
+				numero: '77',
+				nome: 'João de Silva',
+				partido: 'TdoB',
+				vice: 'António de Paula',
 				fotos: [
-					{url: 'adrian.jpg', legenda: 'Prefeito',},
-					{url: 'filho-adrian.jpg', legenda: 'Vice-Prefeito', small: true}
+					{url: '2.png', legenda: 'Prefeito'},
+					{url: '7.png', legenda: 'Vice: António de Paula', small: true}
 				]
 			},
 			{
-				numero: '84',
-				nome: 'Zulano',
-				partido: 'QWERTY',
-				vice: 'Zul',
+				numero: '88',
+				nome: 'Inês Maria',
+				partido: 'ADEBRA',
+				vice: 'Luís Felipe',
 				fotos: [
-					{url: 'mitras.jpg', legenda: 'Prefeito',},
-					{url: 'baruque.jpg', legenda: 'Vice-Prefeito', small: true}
+					{url: '5.png', legenda: 'Prefeito'},
+					{url: '8.png', legenda: 'Vice: Luís Felipe', small: true}
 				]
 			}
 		]
