75da7257fcdb2ac4079947329cbc12f1783f3685
Jomar A. França <jomar.almeida@gmail.com>
2022-05-19 03:34:37 -0300

    Finalizando os arquivos script.js e etapas.js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+	{
+		titulo:'VEREADOR',
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '38111',
+				nome: 'Fulano de Tal',
+				partido: 'ABC',
+				fotos: [
+					{url: 'mulher-begnus.jpg', legenda: 'Vereador'}
+				]
+			},
+			{
+				numero: '77222',
+				nome: 'Beltrano da Silva',
+				partido: 'DEFG',
+				fotos: [
+					{url: 'filha-adrian.jpg', legenda: 'Vereador'}
+				]   
+			},
+		]
+	},
+	{
+		titulo:'PREFEITO',
+		numeros: 2,
+		candidatos: [
+			{
+				numero: '99',
+				nome: 'Ciclano',
+				partido: 'ABC',
+				vice: 'CIC',
+				fotos: [
+					{url: 'adrian.jpg', legenda: 'Prefeito',},
+					{url: 'filho-adrian.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+			},
+			{
+				numero: '84',
+				nome: 'Zulano',
+				partido: 'QWERTY',
+				vice: 'Zul',
+				fotos: [
+					{url: 'mitras.jpg', legenda: 'Prefeito',},
+					{url: 'baruque.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+			}
+		]
+	}
+];
